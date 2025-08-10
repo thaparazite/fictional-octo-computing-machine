ad8bf0c5a825330b261a1267c4282dfd4353a01d
Flaviu Vanca <skety_tm@yahoo.com>
2025-08-10 14:07:59 +0000

    Update siteProps with personal information

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Flaviu Vanca",
+  title: "Software Architect & DevOps",
+  email: "flaviu.vanca@gmail.com",
+  gitHub: "thaparazite",
+  instagram: "flaviu.vanca",
+  linkedIn: "flaviu-vanca",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
